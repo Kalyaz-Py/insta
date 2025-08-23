7f0799457f5b04b707f7bb853d681e3e5f8f7b33
kalyaz ruf <xakimov.rufat.07@gmail.com>
2025-08-23 16:07:35 +0500

    Добавил в андроид студио проект, должно коммититься

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ import 'src/app.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url: 'https://<PROJECT-REF>.supabase.co', // Settings → API → Project URL
-    anonKey: '<ANON-PUBLIC-KEY>',            // Settings → API → anon public
+    url: 'https://hssvwlkhhncgwgluciqz.supabase.co', // Settings → API → Project URL
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhzc3Z3bGtoaG5jZ3dnbHVjaXF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU3OTcxMDQsImV4cCI6MjA3MTM3MzEwNH0.NnAHBx7ndqMw3DtbSLl8yhfgwCy3hMQb8XC6sK2CqZ8',            // Settings → API → anon public
   );
   runApp(const App());
 }
