736024e97e8e61179a1b94b908ef916454adc877
Kalyaz <119740057+Kalyaz-Py@users.noreply.github.com>
2025-08-24 23:51:32 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,8 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'https://okvcvfyrbrevsjvysamy.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rdmN2ZnlyYnJldnNqdnlzYW15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU5NTk0ODEsImV4cCI6MjA3MTUzNTQ4MX0.C6DJJv9uDX8Ckd8Tlhmhu_FoadlPI6f1BXav-LyqD7c',
+    url: '----',
+    anonKey: '----',
     authOptions: const FlutterAuthClientOptions(
       authFlowType: AuthFlowType.pkce, // рекомендовано
     ),
